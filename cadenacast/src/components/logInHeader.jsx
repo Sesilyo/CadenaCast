e45d15b258043a7f801016ce9bf9650e2a542ca6
Sesilyo <c.sethleander@gmail.com>
2025-04-05 23:36:28 +0800

    reworked App and tested components

---
+++ cadenacast/src/components/logInHeader.jsx
@@ -0,0 +1,13 @@
+import logo from '../resources/cadenacast-whitebg.png'
+
+function LogInHeader()
+{
+    return(
+        <>
+            <img src={logo} alt='CadenaCast logo'/>
+            <p>Powered by Jomafa</p>
+        </>
+    );
+}
+
+export default LogInHeader
