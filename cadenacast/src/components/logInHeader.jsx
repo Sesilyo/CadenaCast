b97fa48787e4a531c2c080673543aedbb1496c04
Sesilyo <c.sethleander@gmail.com>
2025-04-06 01:18:48 +0800

    imported Norwester font

--- cadenacast/src/components/logInHeader.jsx
+++ cadenacast/src/components/logInHeader.jsx
@@ -1,9 +1,11 @@
-import logo from '../resources/cadenacast-whitebg.png'
-
+import logo from '../resources/cadenacast-whitebg.png';
+import '../compontent-styles/logInHeader.css';
 function LogInHeader()
 {
     return(
         <>
+            <p>Elections</p>
+            <p>2025</p>
             <img src={logo} alt='CadenaCast logo'/>
             <p>Powered by Jomafa</p>
         </>
