1e246980285a9c14b107fbc6e9927fb82684b7a0
Sesilyo <c.sethleander@gmail.com>
2025-04-06 15:55:21 +0800

    Deleted Firebase files and started testing with React Router

--- cadenacast/src/App.jsx
+++ cadenacast/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import LogInHeader from '../src/components/logInHeader.jsx';
+import LogInHeader from './components/LogInHeader.jsx';
 import LogInForm from '../src/components/LogInForm.jsx';
 
 function App()
